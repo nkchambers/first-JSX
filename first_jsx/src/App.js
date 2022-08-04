4b7241586c74efc0bc1f5a31d4302fff991e0fca
nkchambers <nkchames@gmail.com>
2022-08-03 19:57:30 -0700

    adding first-JSX for 8-3-22

---
+++ first_jsx/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+        <ul>
+          <li>Learn React</li>
+          <li>Go Snowboarding</li>
+          <li>Run 3x/Week</li>
+          <li>Build Desk</li>
+        </ul>
+    </div>
+  );
+}
+
+export default App;
